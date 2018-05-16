dd98e0fc59334f8a5f42fba65db6d2a6fe83c0f5
Edwin Jones <edwinjones@outlook.com>
2018-05-16 21:05:06 +0100

    convert to current time clock

--- pyclock/__main__.pyw
+++ pyclock/__main__.pyw
@@ -10,7 +10,7 @@ screen_size = (400, 400)
 center = (200, 200)
 
 # create an instance of our helper class.
-analog_clock = AnalogClock(center)
+analog_clock = AnalogClock(center, 150)
 
 # create a pygame clock for calculating how much time has passed etc.
 pygame_clock = pygame.time.Clock()
@@ -20,24 +20,27 @@ screen = pygame.display.set_mode(screen_size)
 
 
 # this is a small helper function we use to keep the code tidy
-def draw(second_hand_end_position, minute_hand_end_position):
+def draw(analog_clock):
 
     # pygame always draws back to front so what we do here is:
     
     # draw a white screen
     screen.fill(white)
 
+    # draw the second hand
+    pygame.draw.line(screen, red, center, analog_clock.second_hand_end_position, 1)
+
+    # draw the minute hand
+    pygame.draw.line(screen, black, center, analog_clock.minute_hand_end_position, 5)
+
+    # draw the hour hand
+    pygame.draw.line(screen, black, center, analog_clock.hour_hand_end_position, 5)
+
     # draw the center 'dot' of the clock'
     pygame.draw.circle(screen, black, center, 10)
 
     # draw the outer line of the clock.
     pygame.draw.circle(screen, black, center, 150, 1)
-
-    # draw the second hand
-    pygame.draw.line(screen, red, center, second_hand_end_position, 5)
-
-    # draw the minute hand
-    pygame.draw.line(screen, red, center, minute_hand_end_position, 5)
 
     # we have to call this for the display to update.
     pygame.display.flip()
@@ -50,15 +53,14 @@ pygame.init()
 running = True
 while running:
 
-    # .tick will make this loop run at a maximum speed of the passed in seconds value.
-    # it also returns the elapsed time in milliseconds since the last .tick call.
-    delta_time = pygame_clock.tick(60)
+    # .tick will make this loop run at a maximum speed of the passed in seconds value.    
+    pygame_clock.tick(30)
 
-    # update the analog clock with the elapsed time.
-    analog_clock.update(delta_time)
+    # update the analog clock
+    analog_clock.update()
 
     # draw the hands with the calculated end positions.
-    draw(analog_clock.second_hand_end_position, analog_clock.minute_hand_end_position)  
+    draw(analog_clock)  
 
     # check for any exit button presses and quit the program if any are found.
     for event in pygame.event.get():
--- pyclock/analog_clock.py
+++ pyclock/analog_clock.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pygame.math import Vector2
 
 
@@ -6,43 +7,45 @@ class AnalogClock:
 
     second_hand_end_position = Vector2()
     minute_hand_end_position = Vector2()
+    hour_hand_end_position = Vector2()
 
-    _start_hand_end_position = Vector2(0, -150) #150 will be default length of a hand
-    _second_angle = 0
-    _minute_angle = 0 
-    _angle_per_second = 360 / 60
-    _angle_per_minute = (360 / 60) / 60
+    def __init__(self, center, radius):
+        
+        #store the location of the center of the clock
+        self._center = center
+
+        # negative radius means the vector starts point up/at 12
+        self._start_hand_end_position = Vector2(0, -radius)
 
 
-    def __init__(self, center):
-        self.center = center
+    def _get_angle(self, angle_increment, time_part):
+        """get the angle we should rotate by based on the time part (secs/mins/hrs)"""
 
-
-    def _get_angle(self, angle, angle_increment, delta_time_milliseconds):
-        """get the angle we should rotate by based on the time passed in milliseconds"""
-
-        # clamp angle
-        if angle >= 360:
-            angle = 0
-
-        # calculate the angle, plus the incremement size calculated with the elapsed time
-        angle = angle + (angle_increment / 1000 * delta_time_milliseconds)
+        # calculate the angle, plus the incremement size calculated with secs/mins/hrs
+        angle = 360 / angle_increment * time_part
 
         return angle
 
 
-    def update(self, delta_time):
-        """Update the analog clock. Takes in the amount of milliseconds that have passed since the last call."""
+    def update(self):
+        """Update the analog clock."""
+
+        # get the current local system time
+        time = datetime.now().time()
 
         # calculate the angles of the hands 
-        self._second_angle = self._get_angle(self._second_angle, self._angle_per_second, delta_time)
-        self._minute_angle = self._get_angle(self._minute_angle, self._angle_per_minute, delta_time)
+        second_angle = self._get_angle(60, time.second)
+        minute_angle = self._get_angle(60, time.minute)
+        hour_angle = self._get_angle(12, time.hour)
         
         # calculate the end positions of the hands.
         # we cheat a little here by dividing the results to make them shorter than the clock's radius.
-        self.second_hand_end_position = self._start_hand_end_position.rotate(self._second_angle) / 1.25
-        self.minute_hand_end_position = self._start_hand_end_position.rotate(self._minute_angle) / 2
+        self.second_hand_end_position = self._start_hand_end_position.rotate(second_angle) / 1.25
+        self.minute_hand_end_position = self._start_hand_end_position.rotate(minute_angle) / 1.5
+        self.hour_hand_end_position = self._start_hand_end_position.rotate(hour_angle) / 2
 
-        # We have to add the center of the clock to the results to make sure the hands have the correct origin
-        self.second_hand_end_position = self.second_hand_end_position + self.center
-        self.minute_hand_end_position = self.minute_hand_end_position + self.center
+        # We have to add the center of the clock to the results to make sure 
+        # the hands have the correct origin
+        self.second_hand_end_position += self._center
+        self.minute_hand_end_position += self._center
+        self.hour_hand_end_position += self._center
